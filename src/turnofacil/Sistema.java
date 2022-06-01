47c34e2c5b21ae5e2bbfa7fbf4f70937ef43982a
Agustin-Zucchi <104474333+Agustin-Zucchi@users.noreply.github.com>
2022-06-01 18:24:27 -0300

    mejora login y arreglo add turno.
    cambios:
     -add se cambio completamente, revisar codigo, explica ajustes.
     -login resuelto desde el sistema, podria pasarse al main pero me parece una mejor practica realizarlo desde este apartado.

--- src/turnofacil/Sistema.java
+++ src/turnofacil/Sistema.java
@@ -18,12 +18,24 @@ public class Sistema {
 	
 	public void addTurno(Medico m,Paciente p, LocalDate fecha) {
 	 Turno t = new Turno( 0,m, fecha);
-	 m.addTurno(t); // revisar (arreglar)
-	 p.addTurno(t);
+	 Medico b = medicos.get(medicos.indexOf(m));  //creo medico auxliar y lo copio al medico de la lista que es igual al que ingreso el usuario
+	 													//atravez del indexof encuentro la pos del objeto
+	 b.addTurno(t);		//agrego el turno al medico de la lista
+	 medicos.remove(medicos.indexOf(m));	//borro el medico para no duplicarlo
+	 medicos.add(b);	//agrego a la lista
+	 Paciente e = pacientes.get(pacientes.indexOf(p));
+	 e.addTurno(t);
+	 pacientes.remove(pacientes.indexOf(p));
+	 pacientes.add(e); 
 	}
 	
-	public boolean loginMedico(String Usuario, String Clave) {
-			//craer codigo
-		return true;
+	public void loginMedico(String Usuario, String Clave) {
+		Medico M = null;
+		for(int i=0; i<medicos.size(); i++) {
+			if(Usuario == medicos.get(i).getUsuario())
+				M = medicos.get(i);
+		}
+		if(M.esClave(Clave))
+			M.Ejecuto();
 	}
 }
